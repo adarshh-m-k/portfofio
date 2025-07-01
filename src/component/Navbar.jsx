8845e8f5d932b53334cc75374f773cf24189c521
Adarsh <mkadarsh150@gmail.com>
2025-07-01 15:18:25 +0530

    changed the skills

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,7 +8,7 @@ const navItems = [
     { name: "About", href: "#about" },
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
-    { name: "Contact", href: "#contacts" },
+    { name: "Contact", href: "#contact" },
 ];
 
 function Navbar() {
